d58dcdb1f2fd434b1f20ef20e3cbb623cb485f30
Gustavo Rangel <gustavorangel91@gmail.com>
2022-04-01 14:37:42 -0300

    Some refactoring.

--- src/fire_escaper.ts
+++ src/fire_escaper.ts
@@ -1,122 +1,128 @@
-import inputReader = require('wait-console-input')
-import {Position} from "./position"
-import {MapDrawer, MapSymbols} from  "./map_drawer"
+const inputReader = require("wait-console-input");
+import { Position } from "./position";
+import { MapDrawer, MapSymbols } from "./map_drawer";
 
 export class NextPosition extends Position {
-    symbol: MapSymbols;
+  symbol: MapSymbols;
 
-    constructor(row: number, col: number, symbol: MapSymbols) {
-        super(row, col);
-        this.symbol = symbol;
-    }
+  constructor(row: number, col: number, symbol: MapSymbols) {
+    super(row, col);
+    this.symbol = symbol;
+  }
 }
 
-export enum Direction {
-    Left = "Left",
-    Right = "Right",
-    Up = "Up",
-    Down = "Down",
+export type Direction = "left" | "right" | "up" | "down";
+
+export type NextPositionFunc = (row: number, col: number) => NextPosition;
+
+export type DirectionOrder = [Direction, Direction, Direction, Direction];
+
+function nextPositionLeft(row: number, col: number) {
+  return new NextPosition(row, col - 1, MapSymbols.LeftArrow);
 }
 
-export type NextPositionFunc = (row: number, col: number) => NextPosition
+function nextPositionUp(row: number, col: number) {
+  return new NextPosition(row - 1, col, MapSymbols.UpArrow);
+}
+
+function nextPositionRight(row: number, col: number) {
+  return new NextPosition(row, col + 1, MapSymbols.RightArrow);
+}
+
+function nextPositionDown(row: number, col: number) {
+  return new NextPosition(row + 1, col, MapSymbols.DownArrow);
+}
+
+const directionsFunctionMap: { [key: string]: NextPositionFunc } = {
+  left: nextPositionLeft,
+  up: nextPositionUp,
+  right: nextPositionRight,
+  down: nextPositionDown,
+};
 
 export class FireEscaper {
-    entrance: Position;
-    drawer: MapDrawer;
-    roomMap: MapSymbols[][];
-    maxRowIdx: number;
-    maxColIdx: number;
-    visited: boolean[][];
-    directions: Direction[];
-    directionsFunctionsMap;
+  entrance: Position;
+  drawer: MapDrawer;
+  roomMap: MapSymbols[][];
+  maxRowIdx: number;
+  maxColIdx: number;
+  visited: boolean[][];
+  directions: DirectionOrder;
 
-    constructor(entrance: Position, drawer: MapDrawer, directions: Direction[]) {
-        this.entrance = entrance;
-        this.drawer = drawer;
-        this.roomMap = drawer.mapObjects;
-    
-        this.maxRowIdx = this.roomMap.length - 1;
-        this.maxColIdx = this.roomMap[0].length - 1;
+  constructor(
+    entrance: Position,
+    drawer: MapDrawer,
+    directions: DirectionOrder
+  ) {
+    this.entrance = entrance;
+    this.drawer = drawer;
+    this.roomMap = drawer.mapObjects;
 
-        this.directions = directions;
+    this.maxRowIdx = this.roomMap.length - 1;
+    this.maxColIdx = this.roomMap[0].length - 1;
 
-        this.directionsFunctionsMap = {
-            [Direction.Left]: this.nextPositionLeft,
-            [Direction.Right]: this.nextPositionRight,
-            [Direction.Up]: this.nextPositionUp,
-            [Direction.Down]: this.nextPositionDown,
-        }
+    this.directions = directions;
 
-        this.visited = new Array(this.roomMap.length).fill(false).map(() => new Array(this.roomMap[0].length).fill(false))
+    this.visited = new Array(this.roomMap.length)
+      .fill(false)
+      .map(() => new Array(this.roomMap[0].length).fill(false));
+  }
+
+  consoleInteraction(pos: Position, symbol: MapSymbols) {
+    console.clear();
+    this.drawer.displayMap(pos, symbol);
+    inputReader.wait("Press a button to continue");
+  }
+
+  getNextPositions(row: number, col: number): NextPosition[] {
+    let nextPositions: NextPosition[] = [];
+
+    for (const dir of this.directions) {
+      let nextPositionFunc: NextPositionFunc = directionsFunctionMap[dir];
+      nextPositions.push(nextPositionFunc(row, col));
     }
 
-    consoleInteraction(pos: Position, symbol: MapSymbols) {
-        console.clear()
-        this.drawer.displayMap(pos, symbol)
-        inputReader.wait('Press a button to continue')
+    return nextPositions;
+  }
+
+  recursionSolver(curr_pos: Position, pos_candidate: NextPosition): boolean {
+    let row: number = pos_candidate.row;
+    let col: number = pos_candidate.col;
+
+    this.consoleInteraction(curr_pos, pos_candidate.symbol);
+
+    if (row < 0 || row > this.maxRowIdx || col < 0 || col > this.maxColIdx) {
+      return false;
     }
 
-    nextPositionLeft(row: number, col: number) {
-        return new NextPosition(row, col - 1, MapSymbols.LeftArrow)
+    let objectInPosition: MapSymbols = this.roomMap[row][col];
+
+    if (objectInPosition === MapSymbols.Wall) {
+      return false;
+    }
+    if (objectInPosition === MapSymbols.Fire) {
+      return false;
+    }
+    if (this.visited[row][col]) {
+      return false;
+    }
+    if (objectInPosition === MapSymbols.End) {
+      return true;
     }
 
-    nextPositionUp(row: number, col: number) {
-        return new NextPosition(row - 1, col, MapSymbols.UpArrow)
-    }
+    this.visited[row][col] = true;
+    this.roomMap[row][col] = MapSymbols.Visited;
+    let next_pos = new Position(row, col);
 
-    nextPositionRight(row: number, col: number) {
-        return new NextPosition(row, col + 1, MapSymbols.RightArrow)
-    }
+    this.consoleInteraction(next_pos, MapSymbols.Cursor);
 
-    nextPositionDown(row: number, col: number) {
-        return new NextPosition(row + 1, col, MapSymbols.DownArrow)
-    }
+    let nextPositions = this.getNextPositions(row, col);
 
-    getNextPositions(row: number, col: number): NextPosition[] {
-        let nextPositions: NextPosition[] = [];
-        
-        for (const dir of this.directions) {
-            let nextPositionFunc: NextPositionFunc = this.directionsFunctionsMap[dir];
-            nextPositions.push(nextPositionFunc(row, col))
-        }
-
-        return nextPositions;
-    }
-
-    recursionSolver(curr_pos: Position, pos_candidate: NextPosition): boolean {
-        let row: number = pos_candidate.row;
-        let col: number = pos_candidate.col;
-
-        this.consoleInteraction(curr_pos, pos_candidate.symbol)
-
-        if (row < 0 || row > this.maxRowIdx || col < 0 || col > this.maxColIdx) {
-            return false
-        }
-
-        let objectInPosition: MapSymbols = this.roomMap[row][col]
-
-        if (objectInPosition === MapSymbols.Wall) {
-            return false
-        }
-        if (objectInPosition === MapSymbols.Fire) {
-            return false
-        }
-        if (this.visited[row][col]) {
-            return false
-        }
-        if (objectInPosition === MapSymbols.End) {
-            return true
-        }
-
-        this.visited[row][col] = true;
-        this.roomMap[row][col] = MapSymbols.Visited;
-        let next_pos = new Position(row, col)
-
-        this.consoleInteraction(next_pos, MapSymbols.Cursor)
-
-        let nextPositions = this.getNextPositions(row, col)
-
-        return (this.recursionSolver(next_pos, nextPositions[0]) || this.recursionSolver(next_pos, nextPositions[1]) ||
-                this.recursionSolver(next_pos, nextPositions[2]) || this.recursionSolver(next_pos, nextPositions[3]))
-    }
+    return (
+      this.recursionSolver(next_pos, nextPositions[0]) ||
+      this.recursionSolver(next_pos, nextPositions[1]) ||
+      this.recursionSolver(next_pos, nextPositions[2]) ||
+      this.recursionSolver(next_pos, nextPositions[3])
+    );
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
-import {MapCreator} from  "./map_creator"
-import {MapDrawer, MapSymbols} from  "./map_drawer"
-import {FireEscaper, Direction, NextPosition} from "./fire_escaper"
+import { MapCreator } from "./map_creator";
+import { MapDrawer, MapSymbols } from "./map_drawer";
+import { FireEscaper, NextPosition, DirectionOrder } from "./fire_escaper";
 
 function main() {
-    let width: number = 6;
-    let height: number = width;
+  let width: number = 6;
+  let height: number = width;
 
-    const creator: MapCreator = new MapCreator(height, width)
+  const creator: MapCreator = new MapCreator(height, width);
 
-    const possiblePath = creator.createValidPath()
+  const possiblePath = creator.createValidPath();
 
-    const drawer = new MapDrawer(height, width, possiblePath)
+  const drawer = new MapDrawer(height, width, possiblePath);
 
-    let entrance = possiblePath[0]
+  let entrance = possiblePath[0];
 
-    // non-efficient ordering
-    let directionOrder: Direction[] = [Direction.Left, Direction.Up, Direction.Down, Direction.Right]
-    // efficient ordering
-    // let directionOrder: Direction[] = [Direction.Down, Direction.Right, Direction.Up, Direction.Right]  
+  // non-efficient ordering
+  let directionOrder: DirectionOrder = ["left", "up", "down", "right"];
+  // efficient ordering
+  // let directionOrder: Direction[] = [Direction.Down, Direction.Right, Direction.Up, Direction.Right]
 
-    let escaper = new FireEscaper(entrance, drawer, directionOrder)
+  let escaper = new FireEscaper(entrance, drawer, directionOrder);
 
-    escaper.recursionSolver(entrance, new NextPosition(entrance.row, entrance.col, MapSymbols.Cursor))
+  escaper.recursionSolver(
+    entrance,
+    new NextPosition(entrance.row, entrance.col, MapSymbols.Cursor)
+  );
 }
 
-main()
+main();
